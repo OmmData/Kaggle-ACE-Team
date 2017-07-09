9b4c76677197d8991fceae23296e3374691cede7
OmmData <omargraja@outlook.fr>
2017-07-09 15:26:09 -0700

    Update RF+Titanic.py

--- RF+Titanic.py
+++ RF+Titanic.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[31]:
+# In[1]:
 
 import pandas as pd
 import numpy as np
@@ -9,7 +9,7 @@ import seaborn as sns
 get_ipython().magic(u'matplotlib inline')
 
 
-# In[32]:
+# In[2]:
 
 import csv ##
 with open("train.csv","rb") as source:
@@ -21,7 +21,7 @@ with open("train.csv","rb") as source:
 df = pd.read_csv('train_res')
 
 
-# In[34]:
+# In[3]:
 
 df.loc[df['Sex'] =='male', 'Sex'] = 1.0    #Change Sex values : male-->1 ; female-->0
 df.loc[df['Sex'] =='female', 'Sex'] = 0.0
@@ -32,31 +32,31 @@ df = df[pd.notnull(df['Embarked'])] #drop the row if the Embarked value is nan
 #print(df.to_string())
 
 
-# In[35]:
+# In[4]:
 
 sns.pairplot(df,hue='Survived',palette='Set1')
 
 
-# In[36]:
+# In[5]:
 
 from sklearn.model_selection import train_test_split
 X = df.drop('Survived',axis=1)
 y = df['Survived']
 
 
-# In[37]:
+# In[7]:
 
 ## Train Test Split
 
 #Let's split up the data into a training set and a test set!
 
 
-# In[38]:
+# In[8]:
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
 
 
-# In[39]:
+# In[9]:
 
 ## Random Forests
 
@@ -65,28 +65,28 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
 #(just think about how the splits are done at each node). 
 
 
-# In[40]:
+# In[10]:
 
 from sklearn.ensemble import RandomForestClassifier
 rfc = RandomForestClassifier(n_estimators=100)
 rfc.fit(X_train, y_train)
 
 
-# In[45]:
+# In[11]:
 
 ## Prediction and Evaluation 
 
 #Let's evaluate our RF.
 
 
-# In[46]:
+# In[12]:
 
 from sklearn.metrics import classification_report,confusion_matrix
 rfc_pred = rfc.predict(X_test)
 print(confusion_matrix(y_test,rfc_pred))
 
 
-# In[47]:
+# In[13]:
 
 print(classification_report(y_test,rfc_pred))
 
